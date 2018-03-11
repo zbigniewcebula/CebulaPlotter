07d0ce7f462b7a8827db1afba7a705a46c96a575
Zbigniew Cebula <rrrafalon@gmail.com>
2018-03-11 23:12:01 +0100

    OnionMilk logo, parsing info, log update, spread, objs update
    Added internal onionmilk logo. 
    Added parsing info on left of page. 
    Log now updates without doubled infos. 
    Spread value now lets control spreadiness of points. 
    Objects have updated wireframe.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ var	container;
 var	camera;
 var moveSpeed;
 var	scene;
+var light;
 var	renderer;
+var raycaster;
 
 var axes;
 var grid;
@@ -26,12 +28,28 @@ var mouse	= {
 var pressedKey	= {};
 
 var loadedFile	= null;
+var parsedData	= null;
+
+var spreadSize	= 0;
 
 var logEntries	= new Array();
+var lastLogHash	= 0;
 
-function lerp(a,  b,  t) {
+function lerp(a, b, t) {
 	return a + t * (b - a);
 }
+
+String.prototype.hashCode = function() {
+	var hash = 0;
+	var chr;
+	if (this.length === 0) return hash;
+	for(var i = 0; i < this.length; i++) {
+		chr		= this.charCodeAt(i);
+		hash	= ((hash << 5) - hash) + chr;
+		hash	|= 0;
+	}
+	return hash;
+};
 
 function onMouseMove(event) {
 	mouse.deltaX	= mouse.relX;
@@ -54,7 +72,7 @@ function onMouseUp(event) {
 }
 function onMouseWheel(event) {
 	mouse.wheel	= ((event.deltaY || -event.wheelDelta || event.detail)) || 1;
-	radius		+= mouse.wheel * (pressedKey[16]? 2: 1);
+	radius		+= mouse.wheel * (pressedKey[16]? 0.5: 0.25);
 }
 
 function onKeyDown(event) {
@@ -99,8 +117,121 @@ function processFile() {
 			_msg			+= "<b>File is ready!!!</b><br />";
 			addLogEntry("FileUpload", "File loaded succesfully!");
 			document.getElementById("upload_info").innerHTML	= _msg;
+
+			if (loadedFile.type == "text/plain") {
+				parsedData	= processTXT();
+			}
 		};
 		reader.readAsText(loadedFile);
+	}
+}
+
+function processTXT() {
+	if (loadedFile != null) {
+		if (loadedFile.data != null) {
+			var	tempData	= new Array();
+			var	lines		= loadedFile.data.split("\r\n");
+			var line		= null;
+
+			var	success		= 0;
+			var	fail		= 0;
+			
+			var max			= new Array(3);
+			var min			= new Array(3);
+			for(var l = 0; l < lines.length; ++l) {
+				if (lines[l].length > 0) {
+					line	= lines[l].split(" ");
+					if (line.length == 6) {
+						console.log(line);
+						var geometry	= new THREE.SphereGeometry(1, 6, 5);
+						var material	= new THREE.MeshLambertMaterial({
+							color:	0xFFFFFF
+						});
+						material.color		= new THREE.Color(parseFloat(line[3]), parseFloat(line[4]), parseFloat(line[5]));
+						var sphere			= new THREE.Mesh(geometry, material);
+						sphere.position.x	= parseFloat(line[0]) * spreadSize;
+						sphere.position.y	= parseFloat(line[1]) * spreadSize;
+						sphere.position.z	= parseFloat(line[2]) * spreadSize;
+
+						var geo				= new THREE.EdgesGeometry(geometry);
+						var mat				= new THREE.LineBasicMaterial({
+							color: 0xffffff,
+							linewidth: 5
+						});
+						var wireframe			= new THREE.LineSegments(geo, mat);
+						wireframe.position.x	= sphere.position.x;
+						wireframe.position.y	= sphere.position.y;
+						wireframe.position.z	= sphere.position.z;
+
+						tempData.push({
+							mesh:	sphere,
+							wire:	wireframe,
+							added:	false
+						});
+
+						success	+= 1;
+
+						if (success == 1) {
+							max[0]	= sphere.position.x;
+							max[1]	= sphere.position.y;
+							max[2]	= sphere.position.z;
+
+							min[0]	= sphere.position.x;
+							min[1]	= sphere.position.y;
+							min[2]	= sphere.position.z;
+						} else {
+							max[0]	= Math.max(max[0], sphere.position.x);
+							max[1]	= Math.max(max[1], sphere.position.y);
+							max[2]	= Math.max(max[2], sphere.position.z);
+
+							min[0]	= Math.min(min[0], sphere.position.x);
+							min[1]	= Math.min(min[1], sphere.position.y);
+							min[2]	= Math.min(min[2], sphere.position.z);
+						}
+						/*
+						//Debug
+						addLogEntry("ProcessingTXT", "Added point: " + l + " => at: ("
+							+ sphere.position.x + "; " + sphere.position.y + "; " + sphere.position.z
+							+ ")"
+						);
+						*/
+					} else {
+						addLogEntry("ProcessingTXT", "Line is corrupted, ignoring...");
+						fail	+= 1;
+					}
+				}
+			}
+			addLogEntry("ProcessingTXT",
+					"Successfully load points: " + success
+				+	", Failed: " + fail
+				+	", Total: " + (success + fail)
+			);
+
+			var	_msg	= "<table>";
+
+			_msg	+= "<tr>";
+			_msg		+= "<td><b>Total amount: </b></td><td><b>" + success + "</b></td></td>";
+			_msg	+= "</tr>";
+			_msg	+= "<tr>";
+			_msg		+= "<td>Failed amount: </b></td><td>" + fail + "</td>";
+			_msg	+= "</tr>";
+			_msg	+= "<tr>";
+			_msg		+= "<td>Success amount: </b></td><td>" + success + "</td>";
+			_msg	+= "</tr>";
+
+			_msg	+= "<tr></tr>";
+
+			_msg	+= "<tr>";
+			_msg		+= "<td>Minimals: </b></td><td>" + min[0] + ";" + min[1] + ";" + min[2] + "</td>";
+			_msg	+= "</tr>";
+			_msg	+= "<tr>";
+			_msg		+= "<td>Maximals: </b></td><td>" + max[0] + ";" + max[1] + ";" + max[2] + "</td>";
+			_msg	+= "</tr>";
+
+			_msg		+= "</table>";
+			document.getElementById("parse_info").innerHTML	= _msg;
+			return tempData;
+		}	
 	}
 }
 
@@ -134,65 +265,77 @@ function onLoadFile(event) {
 }
 
 function restartCamera() {
-	cameraCenterPos.set(0, 0, 0);
+	cameraCenterPos.set(spreadSize / 2, 0, spreadSize / 2);
 	theta	= Math.PI / 4;
 	phi		= Math.PI / 4;
-	addLogEntry("Camera", "Camera position and rotation reset.");
+	if (lastLogHash != 0) {
+		addLogEntry("Camera", "Camera position and rotation reset.");
+	}
 }
 
 function addLogEntry(title, msg) {
-	logEntries.push([
-		"[" + title + "]", msg
-	]);
-	var	content	= "<table>";
-	for(var i = 10; i < 10; ++i) {
-		if ((logEntries.length - i) < 0)	continue;
-		content	+= "<tr>";
+	if ((title + msg).hashCode() != lastLogHash) {
+		logEntries.push([
+			"[" + title + "]", msg
+		]);
+		lastLogHash	= (title + msg).hashCode();
 
-		content	+= "<td>" + logEntries[logEntries.length - i][0] + "</td>";		
-		content	+= "<td>" + logEntries[logEntries.length - i][1] + "</td>";		
+		var	content	= "<table>";
+		for(var i = Math.max(0, logEntries.length - 10); i < logEntries.length; ++i) {
+			content	+= "<tr>";
 
-		content	+= "</tr>";
+			content	+= "<td>" + logEntries[i][0] + "</td>";
+			content	+= "<td>" + logEntries[i][1] + "</td>";		
+
+			content	+= "</tr>";
+		}
+		content		+= "</table>";
+		document.getElementById("log").innerHTML	= content;
 	}
-	content		+= "</table>";
-	document.getElementById("log").innerHTML	= content;
+}
+
+function setSpreadSize(size) {
+	spreadSize		= size;
+
+	grid			= new THREE.GridHelper(spreadSize, 100 / spreadSize);
+	grid.position.x	= spreadSize / 2;
+	grid.position.z	= spreadSize / 2;
+	grid.name		= "GRID";
+	scene.remove("GRID");
+	scene.add(grid);
+
+	axes		= new THREE.AxesHelper(spreadSize);
+	axes.position.set(0, 0.05, 0);
+	axes.name	= "AXES";
+	scene.remove("AXES");
+	scene.add(axes);
 }
 
 function init() {
 	cameraTargetPos	= new THREE.Vector3(0, 0, 0);
 	cameraCenterPos	= new THREE.Vector3(0, 0, 0);
-	radius			= 50;
+	radius			= 20;
 	theta			= 0;
 	phi				= 0;
 
 	container	= document.getElementById('container');
-	camera		= new THREE.PerspectiveCamera(60, window.innerWidth / (window.innerHeight * 0.98), 1, 10000);
-	moveSpeed	= 100;
-	restartCamera();
 	scene		= new THREE.Scene();
 	renderer	= new THREE.WebGLRenderer();
+	camera		= new THREE.PerspectiveCamera(60, window.innerWidth / (window.innerHeight * 0.98), 1, 10000);
+	moveSpeed	= 10;
+	setSpreadSize(10);
+	restartCamera();
 	renderer.setSize(window.innerWidth, (window.innerHeight * 0.98));
 	container.appendChild(renderer.domElement);
 
-	grid		= new THREE.GridHelper(1000, 100);
-	axes		= new THREE.AxesHelper(5000);
-	scene.add(axes);
-	scene.add(grid);
+	light		= new THREE.DirectionalLight(0xffffff);
+	light.position.set(0, 100, 0);
+	light.lookAt(new THREE.Vector3(0, 0, 0));
+	scene.add(light);
 
-	axes.position.set(0, 0.05, 0);
+	raycaster	= new THREE.Raycaster();
 
 	processFile();
-
-	var sphere;
-	sphere = new THREE.Mesh(
-		new THREE.SphereGeometry(10, 16, 8),
-		new THREE.MeshBasicMaterial({
-			color:		"red",
-			wireframe:	true
-		})
-	);
-	sphere.position.set(0, 0, 0);
-	scene.add(sphere);
 
 	container.addEventListener('mousedown', onMouseDown, false);
 	container.addEventListener('mouseup', onMouseUp, false);
@@ -212,6 +355,9 @@ function init() {
 
 	document.getElementById('upload')
 		.addEventListener('change', onLoadFile, false);
+
+
+	addLogEntry("Global", "Application inited.");
 }
 
 var deltaTime	= 0;
@@ -221,6 +367,17 @@ function update(timestamp) {
 	deltaTime	= timer - lastTime;
 	if (isNaN(deltaTime))	deltaTime = 0;
 
+	//Objects
+	if (parsedData != null) {
+		for(var i = 0; i < parsedData.length; ++i) {
+			if (parsedData[i].added == false) {
+				scene.add(parsedData[i].mesh);
+				scene.add(parsedData[i].wire);
+			}
+		}
+	}
+
+	//Control
 	if (mouse.button) {
 		theta	+= mouse.deltaX * 3;
 		phi		+= mouse.deltaY * 3;
@@ -230,7 +387,8 @@ function update(timestamp) {
 		if (phi > Math.PI / 2) {
 			phi = Math.PI / 2;
 		}
-		
+		mouse.deltaX	= 0;
+		mouse.deltaY	= 0;
 	}
 
 	var camLookDir		= cameraCenterPos.clone().sub(cameraTargetPos).normalize();
@@ -277,7 +435,7 @@ function update(timestamp) {
 	camera.position.x	= lerp(camera.position.x, cameraTargetPos.x, deltaTime * 15);
 	camera.position.y	= lerp(camera.position.y, cameraTargetPos.y, deltaTime * 15);
 	camera.position.z	= lerp(camera.position.z, cameraTargetPos.z, deltaTime * 15);
-	grid.position.set(Math.floor(camera.position.x / 10) * 10, 0.01, Math.floor(camera.position.z / 10) * 10);
+	//grid.position.set(Math.floor(camera.position.x / 10) * 10, 0.01, Math.floor(camera.position.z / 10) * 10);
 	camera.lookAt(cameraCenterPos);
 	renderer.render(scene, camera);
 
